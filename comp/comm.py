297a44eda75b9165851fbb380d9a2c208f978084
sophia <scastellarin95@gmail.co>
2014-04-06 10:19:31 -0400

    added _prep_request function to python code
    goal of this function is to clarify when requests
    will be made to the arduino.  This means the arduino
    will wait for the python script to ask for data and then
    read sensor data.

--- comp/comm.py
+++ comp/comm.py
@@ -6,6 +6,8 @@ import serial
 port = None             # the port the arduino is located 
 baud_rate =  None       # the baud rate of the arduino
 
+class RequestError(Exception): pass
+
 def main():
     arduino = serial.Serial(port, baud_rate)
     while True:
@@ -13,6 +15,19 @@ def main():
         pos = determine_attitude(dat)           # Process data
         chng = determine_adjustment(pos)        # Math...
         post_change(arduino, chng)              # Tells arduino the change
+
+def _prep_request(typ):
+    # tells the arduino when we want to send/recieve data so that the 
+    # arduino does not have to take continous sensor readings
+    #   param: typ : String, the type of request, 
+    #       'get' for arduino -> python
+    #       'post' for python -> arduino
+    if (typ == 'get'):
+        con.write(1111)
+    elif (typ == 'post'):
+        con.write(0000)
+    else:
+        raise RequestError('invalid request type')
 
 def get_sensor_data(con):
     # gets sensor data
@@ -23,6 +38,7 @@ def get_sensor_data(con):
     #   the same instane.  This method will have to build a dict (subject
     #   to change) with the sensor data and return it
     #   should the ardino return all the sensor data in one request?
+    _prep_request('get')
     data = con.readline()
     return data
 
@@ -46,6 +62,7 @@ def post_change(con, dat):
     # posts needed change to the arduino
     #   param : con : Serial instance, the connection to the arduino
     #   param : dat : tuple, the desired change
+    _prep_request('post')
     con.write(str(dat))
  
 if __name__ == '__main__':
