7d38b3be12f3bbdf396bc574348c78e3c5ddca66
Manraj Tatla <ManrajT@gmail.com>
2014-04-08 22:02:48 -0400

    Wrote and Tested selectLine function (for selecting control line)

--- WatSat_arduino_code.ino
+++ WatSat_arduino_code.ino
@@ -2,21 +2,45 @@
 //Figure out how data for magnetometers have to be handled (photodiodes and temp are just default
 
 
-int inputNum = 0;  //value from 0 to 15, telling which line on the MUX to select and in this form to easily convert to binary that's need for control lines 
+int select = 1;  //value from 0 to 15, telling which line on the MUX to select and in this form to easily convert to binary that's need for control lines 
 
 
+int control0 = 0;
+int control1 = 1;
+int control2 = 2;
+int control3 = 3;
 
 void setup() {
   Serial.begin(9600);
-
+  
+  pinMode(control0, OUTPUT); 
   pinMode(control1, OUTPUT); 
   pinMode(control2, OUTPUT); 
   pinMode(control3, OUTPUT); 
-  pinMode(control4, OUTPUT); 
-
+  
+  
+  Serial.println("hello"); 
+  
 }
 
 void loop() {
+  for(int i=1; i<16; i++){
+    Serial.println(i);
+    /*
+    String stringRep = String(i); 
+    Serial.println(stringRep[0]);
+    delay(100);  
+    */
+    selectLine(i); 
+    Serial.print(digitalRead(control3)); 
+    Serial.print(digitalRead(control2)); 
+    Serial.print(digitalRead(control1)); 
+    Serial.print(digitalRead(control0)); 
+    Serial.println(); 
+    delay(1000);  
+  }
+  
+  /*
   Serial.println((value>>n) & 1
   
   // checks for if anything needs to be done 
@@ -28,23 +52,55 @@ void loop() {
       // TODO: activate actuators
     }
   }
-
+  */
 }
 
 
 
-void selectInputs(int inputNum){
+void selectLine(int inputNum){
   //given number of the input lines we'd like, activate the control lines which select them
-  //same input lines will be selected from both MUXes
-  
-  if(inputNum < 16 && inputNum > 0){
+  if(0 < inputNum && inputNum < 16){
     
-  }
-//convert inputLine 
+    if (inputNum >= 8){
+      digitalWrite(control3,HIGH);
+      inputNum = inputNum - 8; 
+    }
+    else{
+      digitalWrite(control3,LOW);
+    }
+     
+    
+    if (inputNum >= 4){
+        digitalWrite(control2,HIGH);
+        inputNum = inputNum - 4;
+    }
+    else{
+        digitalWrite(control2,LOW);
+    }
+    
+    
+    
+    if (inputNum >= 2){
+       digitalWrite(control1,HIGH);
+       inputNum = inputNum - 2;
+    }
+    else{
+      digitalWrite(control1,LOW);
+    }
+    
+    
+    
+    
+    if (inputNum >= 1){
+        digitalWrite(control0,HIGH);
+        inputNum = inputNum - 1;
+    }
+    else{
+        digitalWrite(control0,LOW);
+    }
+    
   
-  if(controlLines
-  digitalWrite(control1);
-  digitalWrite(control2); 
+  }
 }
 
 
@@ -52,23 +108,20 @@ void selectInputs(int inputNum){
 
 
 
-
-
-
-
+/*
 
 float readPhotoDiode(int pin){
-/*
   // read the input on analog pin 0:
   int sensorValue = analogRead(A0);
   // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
   float voltage = sensorValue * (5.0 / 1023.0);
   // print out the value you read:
   Serial.println(voltage);
-*/
+
+
+
   sensorValue = analogRead(pin);
   float voltage = sensorValue * (5.0 / 1023.0); 
   return voltage; 
 }
-
-
+*/
