3c4591f841b08e129ceb6df181bb05276b145115
sophia <scastellarin95@gmail.co>
2014-05-13 20:37:09 -0400

    finished first unittests
    - added open/close methods to comp/comm.py
    - added test for getting sensor data, activate
    - still outstanding is post changes because not
      yet implemented in code

--- test/test_comm.py
+++ test/test_comm.py
@@ -1,11 +1,40 @@
+"""
+integration test for arduino
+    NOTE: requires the arduino to be plugged in
+    TODO: add docopt to make testing more easy...
+"""
+
 import unittest
+from comp.comm import ADCSArduino
 
 class TestComm(unittest.TestCase):
     
     def setUp(self):
-        pass
+        self.ar = ADCSArduino()
 
-    def test_startup(self):
+    def tearDown(self):
+        self.ar.close_port()
+
+    def test_activate(self):
+        self.ar.activate()
+        self.assertEqual(self.ar.arduino.isOpen(), False)
+
+    def test_get_sensor_data(self):
+        self.ar.open_port()
+        data = self.ar.get_sensor_data()
+        # TODO: make this more representative of what the output 
+        # should be, fix it in general
+        self.assertIsNotNone(data)
+        self.ar.close_port()
+
+    def test_post_change(self):
+        self.ar.open_port()
+        da = "placeholder"
+        self.ar.post_change(da)
+        self.ar.close_port()
+
+    def test_workflow(self):
+        # TODO: test full workflow
         pass
 
 if __name__ == "__main__":
