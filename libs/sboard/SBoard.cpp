a70fa862fabfe6fcfbb51d74e131c9d6febc49d0
sophia <scastellarin95@gmail.co>
2014-05-17 19:53:30 -0400

    started putting together class for sensors
    will make accessing data much easier

---
+++ libs/sboard/SBoard.cpp
@@ -0,0 +1,16 @@
+/*
+ May 17, 2014
+ 
+ A module to model the watsat adcs board that consists of 
+    - 2 photodiodes
+    - a temperature sensor
+    - a double axis magnetometer
+*/
+
+#include <Arduino.h>
+#include "SBoard.h"
+
+SBoard::SBoard()
+{
+    int control_lines[] = {}; 
+}
---
+++ libs/sboard/SBoard.h
@@ -0,0 +1,13 @@
+#ifndef SBoard_h
+#define SBoard_h
+
+#include <Arduino.h>
+
+class SBoard
+{
+    public:
+        SBoard();
+
+};
+
+#endif
