85e06d302237cb6dab0d7cc081173ff6c6c1f475
sophia <scastellarin95@gmail.co>
2014-08-06 21:10:01 -0400

    fixed misspelled variables

--- test/test_comm.py
+++ test/test_comm.py
@@ -15,28 +15,26 @@ class TestComm(unittest.TestCase):
         self.ard.close_arduino_port()
 
     def test_activate(self):
-        self.ar.activate()
-        self.assertEqual(self.ar.arduino.isOpen(), False)
+        self.ard.activate()
+        self.assertEqual(self.ard.arduino.isOpen(), False)
 
     def test_get_sensor_data(self):
-        self.ar.open_arduino_port()
-        data = self.ar.get_sensor_data()
+        self.ard.open_arduino_port()
+        data = self.ard.get_sensor_data()
         print data
         self.assertIsNotNone(data)
-        self.ar.close_arduino_port()
+        self.ard.close_arduino_port()
 
     def test_cont_sample(self):
-        self.ar.open_arduino_port()
+        self.ard.open_arduino_port()
         for i in range (5):
-            data = self.ar.get_sensor_data()
+            data = self.ard.get_sensor_data()
             print data
             self.assertIsNotNone(data)
-        self.ar.close_arduino_port()
+        self.ard.close_arduino_port()
 
     def test_post_change(self):
-        self.ar.open_arduino_port()
-        self.ar.post_change(7)
-        self.ar.close_arduino_port()
+        self.ard.post_change(7)
 
 #    def test_workflow(self):
         # TODO: test full workflow
